b265fb04799ebd3525fd47cb9e7e12b9a5ad7901
Pankaj Sharma <pankaj88sharma@gmail.com>
2022-12-17 13:19:08 +0530

    Updates

---
+++ precache-manifest.932ed609983b541c9bbcc255786398b3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881433f2597b786f85c33ca2600b2475",
+    "url": "/smartavvy-tech/index.html"
+  },
+  {
+    "revision": "41b8ca34488ca80a9230",
+    "url": "/smartavvy-tech/static/css/2.f4955334.chunk.css"
+  },
+  {
+    "revision": "bf641331359a2287b804",
+    "url": "/smartavvy-tech/static/css/main.1355c7f4.chunk.css"
+  },
+  {
+    "revision": "41b8ca34488ca80a9230",
+    "url": "/smartavvy-tech/static/js/2.db1eb3fc.chunk.js"
+  },
+  {
+    "revision": "6276d1baea26a70d602059e5f411df23",
+    "url": "/smartavvy-tech/static/js/2.db1eb3fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf641331359a2287b804",
+    "url": "/smartavvy-tech/static/js/main.e8c86668.chunk.js"
+  },
+  {
+    "revision": "70f7825cb0e759d3da81",
+    "url": "/smartavvy-tech/static/js/runtime-main.64fd4f19.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/smartavvy-tech/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/smartavvy-tech/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/smartavvy-tech/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/smartavvy-tech/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/smartavvy-tech/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/smartavvy-tech/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/smartavvy-tech/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/smartavvy-tech/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/smartavvy-tech/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/smartavvy-tech/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/smartavvy-tech/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/smartavvy-tech/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/smartavvy-tech/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/smartavvy-tech/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/smartavvy-tech/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/smartavvy-tech/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/smartavvy-tech/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
